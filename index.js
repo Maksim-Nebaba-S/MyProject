0c81ab57954fc5ea87771d812593e187d74a60d3
Maxim <maksym.nebaba@nure.ua>
2022-09-07 07:42:57 +0300

    first addon

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-34232
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели ? ', '')
+let personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const a = prompt("Один из послдених фильмов", ""),
+      b = prompt("На сколько оцените его ?", ""),
+      c = prompt("Один из послдених фильмов", ""),
+      n = prompt("На сколько оцените его ?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = n;
+
+console.log(personalMovieDB);
