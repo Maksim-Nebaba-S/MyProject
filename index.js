c6e38e55d2c152d19587784447971af32245c7ee
Maxim <maksym.nebaba@nure.ua>
2022-09-09 10:34:09 +0300

    Кино работает

--- index.js
+++ index.js
@@ -6,11 +6,56 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-const a = prompt("Один из послдених фильмов", ""),
-      b = prompt("На сколько оцените его ?", ""),
-      c = prompt("Один из послдених фильмов", ""),
-      n = prompt("На сколько оцените его ?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = n;
+
+if (personalMovieDB.count < 10)
+{
+    alert("Просмотрено довольно мало фильмов");
+    
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert("Вы классический зритель");
+}
+else if ( personalMovieDB.count >=30){
+    alert("Вы киноман");
+}
+else alert("Ошибка");
+
+let a, b, c,d;
+for(let i = 0; i<2; i++){
+      for(let y=0; y<1; y--){
+      a = prompt("Один из последних фильмов", "");
+      if (a !== null && a !== "" && a.length < 50){
+      
+      break;
+      }
+      else {alert("Введите снова");}
+      }
+      for(let u=0; u<1; u--){
+      b = +prompt("На сколько оцените его ?", "");
+      if (b !== null && b !== ""){
+     
+      c = a;
+      d = b;
+      
+      break;
+      }
+      else {
+        alert(b);
+        alert("Введите снова");
+      }
+      
+      
+
+      }
+      
+      personalMovieDB.movies[c] = d;
+    }
+
+
+
 
 console.log(personalMovieDB);
+
+
+
+
